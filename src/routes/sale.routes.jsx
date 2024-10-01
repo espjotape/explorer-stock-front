6e77de652d7bf5ccfd57bb9966ab13632b0bdb29
João Pedro <joaope.espindola2005@gmail.com>
2024-10-01 12:11:09 -0300

    Redirecting Routes According to Profile

--- src/routes/sale.routes.jsx
+++ src/routes/sale.routes.jsx
@@ -3,10 +3,9 @@ import { Routes, Route } from 'react-router-dom';
 import { Home } from '../pages/Home';
 import { Product } from '../pages/Product';
 import { NotFound } from '../pages/NotFound';
-import { Suppliers } from '../pages/Suppliers';
 import { SalesReport } from '../pages/SalesReport';
 
-export function AdminRoutes() {
+export function SaleRoutes() {
   return (
     <Routes>
       <Route path="/" element={< Home />} />
