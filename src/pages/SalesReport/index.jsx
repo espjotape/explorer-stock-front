a77c978313e3d47ca2bdd22e927a92da4e416f76
João Pedro <joaope.espindola2005@gmail.com>
2024-10-01 14:49:19 -0300

    Using Cookie in Authorization

--- src/pages/SalesReport/index.jsx
+++ src/pages/SalesReport/index.jsx
@@ -1,6 +1,15 @@
 import { Container } from "./styles";
+import { useEffect } from "react";
+import { api } from "../../services/api"
 
 export function SalesReport() {
+  
+  useEffect(() => {
+    api.get('/sales', { withCredenditals: true })
+    .then((response) => console.log(response.data))
+
+  })
+
   return (
     <Container>
       <h1>Relatório de Vendas</h1>
